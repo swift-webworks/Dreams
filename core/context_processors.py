f55ab555b236af5de5ea68f70d54e10b4f861ce6
swift-webworks <swiftwebworks05@gmail.com>
2026-07-14 10:33:58 +0530

    updated address

--- core/context_processors.py
+++ core/context_processors.py
@@ -8,11 +8,11 @@ def company_info(request):
         "COMPANY_PHONE": "7708155016",
         "COMPANY_PHONE_DISPLAY": "+91 77081 55016",
         "COMPANY_WHATSAPP_LINK": "https://wa.me/917708155016",
-        "COMPANY_EMAIL": "info@dreamstourstravels.in",
-        "COMPANY_ADDRESS_LINE1": "Nearby Temple Road",
-        "COMPANY_ADDRESS_LINE2": "Rameswaram, Tamil Nadu - 623518",
-        "COMPANY_CITY": "Rameswaram",
-        "SITE_URL": getattr(settings, "SITE_URL", "https://www.dreamstourstravels.in"),
+        "COMPANY_EMAIL": "toursandtravelsdreams@gmail.com",
+        "COMPANY_ADDRESS_LINE1": "Easwari Amman Kovil Street",
+        "COMPANY_ADDRESS_LINE2": "Nearby Temple Road",
+        "COMPANY_CITY": "Rameswaram, Tamil Nadu - 623518",
+        "SITE_URL": getattr(settings, "SITE_URL", "https://www.dreamstourstravel.in"),
         "GOOGLE_MAP_EMBED": (
             "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.0!2d79.3129!3d9.2876"
             "!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sRameswaram%2C+Tamil+Nadu!5e0!3m2!1sen!2sin"
